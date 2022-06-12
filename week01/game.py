17604b6eafc975b1cc8656f47ded0a39c8c6d999
Donnovan22 <val22002@byui.edu>
2022-06-12 00:41:56 -0500

    tic tac toe game

--- week01/game.py
+++ week01/game.py
@@ -1,20 +1,27 @@
+#COURSE: CSE210
+#ASSINGMENT: Tic-Tac-Toe
+# AUTHOR: Emanuel Valencia
+# INSTRUCTOR: Brad Lythgoe
+
 import math
+
 
 def main():
     start_game()
 
+
 def start_game():
     print("Welcome to Tic Tac Toe !!!")
-    table = table_builder(3,3)
+    table = table_builder(3, 3)
     whose_turn = 1
-    
+
     while True:
         print_message(whose_turn)
         draw_table(table)
         selected_square = get_square_input()
         perform_move(table, selected_square, whose_turn)
         did_win = verify(table, whose_turn)
-        
+
         if did_win:
             break
 
@@ -28,7 +35,7 @@ def start_game():
 
     print_winning_message(whose_turn)
     draw_table(table)
-        
+
 
 def print_winning_message(player):
     if player == 1:
@@ -39,14 +46,17 @@ def print_winning_message(player):
         return
     print("It's a draw !!!")
 
+
 def print_message(player):
     if player == 1:
         print("x's turn to choose square (1-9)")
     if player == 2:
         print("o's turn to choose square (1-9)")
 
+
 def get_square_input():
     return int(input())
+
 
 def is_playable(table):
     for i in range(len(table)):
@@ -55,11 +65,13 @@ def is_playable(table):
                 return True
 
     return False
-            
+
+
 def table_builder(height, width):
     table = [[0 for k in range(width)] for i in range(height)]
-    
+
     return table
+
 
 def verify(table, player):
     height = len(table)
@@ -69,7 +81,7 @@ def verify(table, player):
         for j in range(width):
             if table[i][j] != player:
                 temp = temp and False
-        
+
         if temp:
             return True
 
@@ -86,7 +98,7 @@ def verify(table, player):
         if table[i][i] != player:
             temp = temp and False
     if temp:
-            return True
+        return True
 
     temp = True
     for i in range(height):
@@ -94,10 +106,8 @@ def verify(table, player):
             temp = temp and False
     if temp:
         return True
-    
+
     return False
-
-
 
 
 def draw_table(table):
@@ -116,10 +126,12 @@ def draw_table(table):
                 line = line + "|O|"
         print(line)
 
+
 def perform_move(table, selectedSquare, player):
     i = math.ceil(selectedSquare / 3) - 1
     j = ((selectedSquare - 1) % 3)
     table[i][j] = player
+
 
 if __name__ == "__main__":
     main()
